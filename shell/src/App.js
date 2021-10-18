01f900760a04caf3990fcc47e90a91ae710ec4f6
boddhi9 <krum.ag@gmail.com>
2021-10-18 22:05:31 +0300

    1 file modified

--- shell/src/App.js
+++ shell/src/App.js
@@ -1,10 +1,10 @@
-import React, { Suspense } from 'react'
-const App1 = React.lazy(() => import('app1/App'))
-const App2 = React.lazy(() => import('app2/App'))
+import React, { Suspense } from 'react';
+const App1 = React.lazy(() => import('app1/App'));
+const App2 = React.lazy(() => import('app2/App'));
 
 const App = () => {
   return (
-    <div style={{ margin: 10, bordeRadius: 10, border: '1px solid #999' }}>
+    <div>
       <Suspense fallback={'loading...'}>
         <App1 />
       </Suspense>
@@ -12,7 +12,7 @@ const App = () => {
         <App2 />
       </Suspense>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
