d586e195de7ba7d55e90dac00d19b1b14cc77157
boddhi9 <krum.ag@gmail.com>
2021-10-18 20:47:52 +0300

    3 files modified

--- header/webpack.config.js
+++ header/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'app2',
+      name: 'header',
       filename: 'remoteEntry.js',
       exposes: {
         './App': './src/App',
