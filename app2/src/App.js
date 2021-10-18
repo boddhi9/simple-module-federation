f97e23863d0eafdbb695faa5d88010e070839339
boddhi9 <krum.ag@gmail.com>
2021-10-19 01:29:16 +0300

    2 files modified

--- app2/src/App.js
+++ app2/src/App.js
@@ -1,13 +1,9 @@
 import React, { Suspense } from 'react'
 const Content = React.lazy(() => import('content/App'))
-const Search = React.lazy(() => import('search/App'))
 
 const App = () => {
   return (
     <div style={{ margin: 10, bordeRadius: 10, border: '1px solid #999' }}>
-      <Suspense fallback={'loading...'}>
-        <Search />
-      </Suspense>
       <Suspense fallback={'loading...'}>
         <Content />
       </Suspense>
