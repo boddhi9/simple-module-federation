b78cdfe990d41ff55b0e8f68172c9696e8be8517
boddhi9 <krum.ag@gmail.com>
2021-10-18 20:14:41 +0300

    1 file modified

--- app1/webpack.config.js
+++ app1/webpack.config.js
@@ -1,42 +1,42 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin'
-import { container } from 'webpack'
-import ExternalTemplateRemotesPlugin from 'external-remotes-plugin'
-import { join } from 'path'
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { ModuleFederationPlugin } = require('webpack').container
+const ExternalTemplateRemotesPlugin = require('external-remotes-plugin')
+const path = require('path')
 
-const { ModuleFederationPlugin } = container
-
-export const entry = './src/index'
-export const mode = 'development'
-export const devServer = {
-  static: join(__dirname, 'dist'),
-  port: 3001,
-}
-export const output = {
-  publicPath: 'auto',
-}
-export const module = {
-  rules: [
-    {
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-      options: {
-        presets: ['@babel/preset-react'],
+module.exports = {
+  entry: './src/index',
+  mode: 'development',
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    port: 3001,
+  },
+  output: {
+    publicPath: 'auto',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-react'],
+        },
       },
-    },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'app1',
+      remotes: {
+        app2: 'app2@[app2Url]/remoteEntry.js',
+        app3: 'app3@[app3Url]/remoteEntry.js',
+      },
+      shared: { react: { singleton: true }, 'react-dom': { singleton: true } },
+    }),
+    new ExternalTemplateRemotesPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
   ],
 }
-export const plugins = [
-  new ModuleFederationPlugin({
-    name: 'app1',
-    remotes: {
-      app2: 'app2@[app2Url]/remoteEntry.js',
-      app3: 'app3@[app3Url]/remoteEntry.js',
-    },
-    shared: { react: { singleton: true }, 'react-dom': { singleton: true } },
-  }),
-  new ExternalTemplateRemotesPlugin(),
-  new HtmlWebpackPlugin({
-    template: './public/index.html',
-  }),
-]
